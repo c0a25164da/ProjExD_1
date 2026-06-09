842e20a5cb6ff5b9dfa19416ce5c082590653f60
tatsuki <c0a25164da@edu.teu.ac.jp>
2026-06-09 15:55:04 +0900

    練習7と8, 9：背景画像の残像修正

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_trans = pg.transform.flip(bg_img, True, False)
     img_3 = pg.image.load("fig/3.png")
     img_3_trans = pg.transform.flip(img_3, True, False)
     tmr = x = 0
@@ -18,11 +19,15 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [x, 0])
+        x -= 1
+        tmr += 1
+        screen.blit(bg_img_trans, [x + 1600, 0])
+        screen.blit(bg_img, [x + 3200, 0])
+        if x <= -3199:
+            x = 0
         screen.blit(img_3_trans, [300,200])
         pg.display.update()
-        tmr += 1
-        x -= 1       
-        clock.tick(20)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
