98dd3662eb7be5ce2f7badf0e49eabe46e31b809
tatsuki <c0a25164da@edu.teu.ac.jp>
2026-06-09 16:40:51 +0900

    演習１：こうかとんの初期動作追加

--- pygame_image.py
+++ pygame_image.py
@@ -15,23 +15,26 @@ def main():
     img_3_trans = pg.transform.flip(img_3, True, False)
     img_3_trans_rct = img_3_trans.get_rect()
     img_3_trans_rct.center = 300, 200
-    tmr = 0
+    tmr = dx = dy = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        Key_lst = pg.key.get_pressed()
-        if Key_lst[pg.K_UP]:
-            img_3_trans_rct.move_ip((0, -1))
-        elif Key_lst[pg.K_DOWN]:
-            img_3_trans_rct.move_ip((0, 1))
-        elif Key_lst[pg.K_LEFT]:
-            img_3_trans_rct.move_ip((-1, 0))
-        elif Key_lst[pg.K_RIGHT]:
-            img_3_trans_rct.move_ip((1, 0))
-
         x = -(tmr % 3200)
         tmr += 1
+
+        Key_lst = pg.key.get_pressed()
+        if not any(Key_lst):
+            img_3_trans_rct.move_ip((-1, 0))
+        if Key_lst[pg.K_UP]:
+            img_3_trans_rct.move_ip((0, -1))
+        if Key_lst[pg.K_DOWN]:
+            img_3_trans_rct.move_ip((0, 1))
+        if Key_lst[pg.K_LEFT]:
+            img_3_trans_rct.move_ip((-1, 0))
+        if Key_lst[pg.K_RIGHT]:
+            img_3_trans_rct.move_ip((2, 0))
+
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img_trans, [x + 1600, 0])
         screen.blit(bg_img, [x + 3200, 0])
