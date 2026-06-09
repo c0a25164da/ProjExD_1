076eee62196ea303436af4a5c1e144a85a345e07
tatsuki <c0a25164da@edu.teu.ac.jp>
2026-06-09 15:26:57 +0900

    練習3と4：こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    img_3 = pg.image.load("fig/3.png")
+    img_3_trans = pg.transform.flip(img_3, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(img_3_trans, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
